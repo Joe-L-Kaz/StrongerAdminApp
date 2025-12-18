89d24ef2895d2a6ecb4d7e7d03a7d9ff7d297ebf
Joseph Lobo-Kazinczi <joseph.lobo-kazinczi@lampada.co>
2025-12-18 15:14:44 +0000

    removing boiler plate and add stronger logo

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function Login() {
+    return (
+        <div>
+            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                Login Page
+            </h1>
+        </div>
+    );
+}
